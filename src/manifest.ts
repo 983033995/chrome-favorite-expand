723b0c63a8d5431bf32d611578f32dee214076ac
983033995 <983033995@qq.com>
2025-03-21 15:53:34 +0800

    feat: 更新扩展清单文件
    1. 更新扩展权限，添加unlimitedStorage和favicon权限
    2. 增加可访问的资源，用于图标和样式
    3. 优化manifest结构，为后续功能做准备
    4. 添加中文注释，提高代码可读性

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,6 +1,6 @@
 /*
  * @FilePath: /chrome-favorite-expand/src/manifest.ts
- * @Description:
+ * @Description: 扩展清单文件
  */
 import fs from 'fs-extra'
 import type { Manifest } from 'webextension-polyfill'
@@ -10,8 +10,7 @@ import { isDev, isFirefox, port, r } from '../scripts/utils'
 export async function getManifest() {
   const pkg = await fs.readJSON(r('package.json')) as typeof PkgType
 
-  // update this file to update this manifest.json
-  // can also be conditional based on your need
+  // 扩展清单配置
   const manifest: Manifest.WebExtensionManifest = {
     manifest_version: 3,
     name: pkg.displayName || pkg.name,
@@ -34,15 +33,18 @@ export async function getManifest() {
           service_worker: './dist/background/index.mjs',
         },
     icons: {
-      16: './assets/icon-512.png',
-      48: './assets/icon-512.png',
-      128: './assets/icon-512.png',
+      16: './assets/icon-16.png',
+      48: './assets/icon-48.png',
+      128: './assets/icon-128.png',
+      512: './assets/icon-512.png',
     },
     permissions: [
       'tabs',
       'storage',
       'activeTab',
       'bookmarks',
+      'unlimitedStorage',
+      'favicon',
     ],
     host_permissions: ['*://*/*'],
     content_scripts: [
@@ -53,29 +55,31 @@ export async function getManifest() {
         js: [
           'dist/contentScripts/index.global.js',
         ],
+        css: [
+          'dist/contentScripts/style.css',
+        ],
       },
     ],
     web_accessible_resources: [
       {
-        resources: ['dist/contentScripts/style.css'],
+        resources: [
+          'dist/contentScripts/style.css',
+          'assets/icon-16.png',
+          'assets/icon-48.png',
+          'assets/icon-128.png',
+          'assets/icon-512.png',
+          'assets/logo.svg',
+          'assets/icons/*.svg',
+        ],
         matches: ['<all_urls>'],
       },
     ],
     content_security_policy: {
       extension_pages: isDev
-        // this is required on dev for Vite script to load
-        ? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
-        : 'script-src \'self\'; object-src \'self\'',
+        // 开发环境需要允许Vite脚本加载
+        ? `script-src \\'self\\' http://localhost:${port}; object-src \\'self\\'`
+        : 'script-src \\'self\\'; object-src \\'self\\'',
     },
-  }
-
-  // FIXME: not work in MV3
-  if (isDev && false) {
-    // for content script, as browsers will cache them for each reload,
-    // we use a background script to always inject the latest version
-    // see src/background/contentScriptHMR.ts
-    delete manifest.content_scripts
-    manifest.permissions?.push('webNavigation')
   }
 
   return manifest
